63bcb06408b70123e1ae451503b7858fbf13b2c4
Tausandbill <55964147+Tausandbill@users.noreply.github.com>
2020-01-18 20:11:28 -0500

    Add files via upload

---
+++ signup.php
@@ -0,0 +1,28 @@
+<?php
+    require "header.php"
+?>
+
+    <main>
+        <h1>Signup</h1>
+        <form action="includes/signup.inc.php" method="post">
+            <input type="text" name="firstName" placeholder="First Name">
+            <br>
+            <input type="text" name="lastName" placeholder="Last Name">
+            <br>
+            <input type="date" name="dob">
+            <br>
+            <input type="text" name="address" placeholder="Address">
+            <br>
+            <input type="email" name="mail" placeholder="E-Mail">
+            <br>
+            <input type="password" name="pwd" placeholder="Password">
+            <br>
+            <input type="password" name="pwdRepeat" placeholder="Repeat password">
+            <br>
+            <button type="submit" name="signup-submit">Submit</button>
+        </form>
+    </main>
+
+<?php
+    require "footer.php"
+?>
